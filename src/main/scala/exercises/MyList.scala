6eab30f8f8ddf5c547884cae99b21c0be278a130
phillip.kim <phillip.kim@ibm.com>
2022-01-12 19:31:48 +1100

    Anonymous class exercises using previous MyList exercise

--- src/main/scala/exercises/MyList.scala
+++ src/main/scala/exercises/MyList.scala
@@ -1,6 +1,6 @@
 package exercises
 
-// import scala.runtime.Nothing$
+//import scala.runtime.Nothing$
 
 abstract class MyList[+A] {
 
@@ -20,6 +20,11 @@ abstract class MyList[+A] {
 // printElements is a polymorphic call
   def printElements: String
   override def toString: String = "[" + printElements + "]"
+
+  def map[B](transformer: MyTransformer[A, B]): MyList[B]
+  def flatMap[B](transformer: MyTransformer[A, MyList[B]]): MyList[B]
+  def filter(predicate: MyPredicate[A]): MyList[A]
+  def ++[B >: A](list: MyList[B]): MyList[B]
 }
 
 object Empty extends MyList[Nothing] {
@@ -29,18 +34,63 @@ object Empty extends MyList[Nothing] {
   def add[B >: Nothing](element: B): MyList[B] = new Cons(element, Empty)
   def printElements: String = ""
 
+  def map[B](transformer: MyTransformer[Nothing, B]): MyList[B] = Empty
+  def flatMap[B](transformer: MyTransformer[Nothing, MyList[B]]): MyList[B] = Empty
+  def filter(predicate: MyPredicate[Nothing]): MyList[Nothing] = Empty
+  def ++[B >: Nothing](list: MyList[B]): MyList[B] = list
 }
 
 class Cons[+A](h: A, t: MyList[A]) extends MyList[A] {
   def head: A = h
+
   def tail: MyList[A] = t
+
   def isEmpty: Boolean = false
+
   def add[B >: A](element: B): MyList[B] = new Cons(element, this)
+
   def printElements: String = {
     if (t.isEmpty) "" + h
     else s"$h ${t.printElements}"
-//    else h + " " + t.printElements // this errors due to +, does A have + defined?
+    //    else h + " " + t.printElements // this errors due to +, does A have + defined?
   }
+
+  def filter(predicate: MyPredicate[A]): MyList[A] =
+    if (predicate.test(h)) new Cons(h, t.filter(predicate))
+    else t.filter(predicate)
+
+  def map[B](transformer: MyTransformer[A, B]): MyList[B] =
+    new Cons(transformer.transform(h), t.map(transformer))
+
+
+
+  /*
+  [1,2] ++ [3,4,5]
+  = new Cons(1, [2] ++ [3,4,5])
+  = new Cons(1, new Cons(2, Empty ++ [3,4,5]))
+  = new Cons(1, new Cons(2, new Cons(3, new Cons(4, new Cons(5)))))
+  */
+  def ++[B >: A](list: MyList[B]): MyList[B] = new Cons(h, t ++ list)
+
+  /*
+  [1,2].flatMap(n => [n, n+1]
+  = [1,2] ++ [2].flatMap(n => [n, n+1])
+  = [1,2] ++ [2,3] ++ Empty.flatMap(n => [n, n+1])
+  = [1,2] ++ [2,3] ++ Empty
+  = [1,2,2,3]
+  */
+
+  def flatMap[B](transformer: MyTransformer[A, MyList[B]]): MyList[B] =
+    transformer.transform(h) ++ t.flatMap(transformer)
+
+}
+
+trait MyPredicate[-T] {
+  def test(elem: T): Boolean
+}
+
+trait MyTransformer[-A, B] {
+  def transform(elem: A): B
 }
 
 object ListTest extends App {
@@ -52,9 +102,40 @@ object ListTest extends App {
   println(list.toString)*/
 
   val listOfIntegers: MyList[Int] = new Cons(1, new Cons(2, new Cons(3, Empty)))
+  val anotherListOfIntegers: MyList[Int] = new Cons(4, new Cons(5, Empty))
   val listOfStrings: MyList[String] = new Cons("hello", new Cons("Scala", Empty))
 
   println(listOfIntegers.toString)
   println(listOfStrings.toString)
+
+  /*
+  [1,2,3].map(n * 2)
+  = new Cons(2, [2,3].map(n * 2))
+  = new Cons(2, new Cons(4, [3].map(n * 2)))
+  = new Cons(2, new Cons(4, new Cons(6, Empty.maps(n * 2))))
+  = new Cons(2, new Cons(4, new Cons(6, Empty)))
+  */
+
+  println(listOfIntegers.map(new MyTransformer[Int, Int] {
+    override def transform(elem: Int): Int = elem * 2
+  }).toString)
+
+  /*
+  [1,2,3].filter(n % 2 == 0) =
+    [2,3].filter(n % 2 == 0) =
+    = new Cons(2, [3].filter(n % 2 == 0)
+    = new Cons(2, Empty.filter(n % 2 == 0)
+    = new Cons(2, Empty)
+  */
+
+  println(listOfIntegers.filter(new MyPredicate[Int] {
+    override def test(elem: Int): Boolean = elem % 2 == 0
+  }))
+
+  println((listOfIntegers ++ anotherListOfIntegers).toString)
+
+  println(listOfIntegers.flatMap(new MyTransformer[Int, MyList[Int]] {
+    override def transform(elem: Int): MyList[Int] = new Cons(elem, new Cons(elem + 1, Empty))
+  }).toString)
 
 }
--- src/main/scala/lectures/part2oop/AnonymousClasses.scala
+++ src/main/scala/lectures/part2oop/AnonymousClasses.scala
@@ -28,4 +28,32 @@ object AnonymousClasses extends App {
 
   // Anonymous classes work for abstract and non-abstract classes and traits
 
+  /*
+  1. Generic trait MyPredicate[-T]
+  method to test a value of type T passes a condition
+  test(T) => Boolean
+
+  2. Generic trait MyTransformer[-A, B]
+  method to convert type A into type B
+  method transform(A) => B
+  every syblcass of my transformer will have a different implementation of that method
+
+  3. MyList:
+    - map(transformer) => new MyList of a different type
+    - filter(predicate) => MyList
+    - flatMap(transformer from A to MyList[B]) => MyList[B]
+
+    class EvenPredicate extends MyPredicate[Int]
+    class StringToIntTransformer extends MyTransformer[String, Int]
+
+    [1,2,3].map(n * 2) = [2,4,6]
+    [1,2,3,4].filter(n % 2) = [2,4]
+    [1,2,3].flatMap(n => [n, n+1]) => [1,2,2,3,3,4]
+  */
+
+
+
+
+  
+  
 }
